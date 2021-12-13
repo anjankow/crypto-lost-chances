d13995fadb3c9bd4db0874a160e105574198bd8c
Anna <anjankow@protonmail.com>
2021-12-13 10:49:17 +0100

    fix returned codes

--- db-viewer/function.go
+++ db-viewer/function.go
@@ -32,7 +32,7 @@ func ViewDB(w http.ResponseWriter, r *http.Request) {
 	dsn := fmt.Sprint("projects/", projectName, "/instances/", dbInstance, "/databases/", dbName)
 	client, err := spanner.NewClient(ctx, dsn)
 	if err != nil {
-		fmt.Println("error when connecting to the db: ", err.Error())
+		http.Error(w, "error when connecting to the db: "+err.Error(), http.StatusInternalServerError)
 		return
 	}
 	defer client.Close()
@@ -61,34 +61,34 @@ func ViewDB(w http.ResponseWriter, r *http.Request) {
 			break
 		}
 		if err != nil {
-			fmt.Println("error when getting a next result: ", err.Error())
+			http.Error(w, "error when getting a next result: "+err.Error(), http.StatusInternalServerError)
 			continue
 		}
 
 		var out HistoricalPrice
 		if err := row.Columns(&out.CryptocurrencyName, &out.FiatName, &out.MonthYear,
 			&out.PriceHighest, &out.PriceLowest); err != nil {
-			fmt.Println("scanning error: ", err.Error())
+			http.Error(w, "scanning error: "+err.Error(), http.StatusInternalServerError)
 			continue
 		}
 
-		fmt.Println("price: ", out.CryptocurrencyName, out.FiatName, out.MonthYear, out.PriceHighest, out.PriceLowest)
+		fmt.Println(w, "price: ", out.CryptocurrencyName, out.FiatName, out.MonthYear, out.PriceHighest, out.PriceLowest)
 		records = append(records, out)
 	}
 
 	if len(records) == 0 {
-		fmt.Println("no records in the db")
-		fmt.Fprintf(w, "no records in the db")
+		fmt.Fprint(w, "no records in the db")
+
 		return
 	}
 
 	b, err := json.Marshal(records)
 	if err != nil {
-		fmt.Println("failed to marshal the records: " + err.Error())
+		http.Error(w, "failed to marshal the records: "+err.Error(), http.StatusInternalServerError)
 		return
 	}
 
 	if _, err := w.Write(b); err != nil {
-		fmt.Println("failed to write the response: " + err.Error())
+		http.Error(w, "failed to write the response: "+err.Error(), http.StatusInternalServerError)
 	}
 }
